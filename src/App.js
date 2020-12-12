fd7117ced0dfee8534d3e44af4583a6d4698a63e
김찬민 <rlacksals96@gmail.com>
2020-12-12 18:23:21 +0900

    edit:set default btn status true

--- src/App.js
+++ src/App.js
@@ -6,26 +6,32 @@ function App() {
     const [isClock,setIsClock]=useState(true);
     const [isAsmr,setIsAsmr]=useState(true);
     const[isClassic,setIsClassic]=useState(true);
-    const [isTodoList,setIsTodoList]=useState(false);
+    const [isTodoList,setIsTodoList]=useState(true);
     const handleClock=()=>{
+        alert("clock을 구입하였습니다. 100 coin 차감됩니다")
         setIsClock(!isClock)
     }
     const handleAsmr=()=>{
+        alert("ASMR을 구입하였습니다. 100 coin 차감됩니다")
         setIsAsmr(!isAsmr)
     }
     const handleClassic=()=>{
+        alert("classic 음악을 구입하였습니다. 100 coin 차감됩니다")
         setIsClassic(!isClassic)
     }
     const handleTodoList=()=>{
+        alert("todo list를 구입하였습니다. 100 coin 차감됩니다")
         setIsTodoList(!isTodoList)
     }
   return (
     <div className="App">
+        {/*{window.moveTo(0,-1)}*/}
+        {/*{window.resizeTo(500,1000)}*/}
         <div className="btn_bar">
             <h5 id="select_text">원하는 아이템을 선택하세요(아이템당 100coin 차감)</h5>
             <button className="btn_style" onClick={handleClock}>포모도로 시계</button>
             <button className="btn_style" onClick={handleAsmr}>ASMR</button>
-            <button className="btn_style" onClick={handleClassic}>클래식</button>
+            <button className="btn_style" onClick={handleClassic}>클래식 음악</button>
             <button className="btn_style" onClick={handleTodoList}>todo list</button>
 
         </div>
